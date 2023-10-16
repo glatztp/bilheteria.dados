ba8e0c7de432584e633fb2ba9343f92694522415
Glatz <143537581+glatztp@users.noreply.github.com>
2023-10-16 16:23:38 -0300

    Add files via upload

---
+++ ver.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<style type="text/css">
+body {
+    font-family: Arial, sans-serif;
+    background-color: #000;
+    margin: 0;
+    padding: 0;
+    color: #fff;
+}
+
+h2 {
+    text-align: center;
+    color: #ff4500;
+    margin-top: 20px;
+    margin-top: 90px;
+    margin-left: 20;
+}
+
+h1{
+	text-align: center;
+	color: #ff4500;  
+}
+h3{
+    text-align: center;
+}
+
+img {
+    width: 150px;
+    height: auto;
+    border-radius: 5px;
+}
+
+.ranking-container {
+    flex-direction: column;
+    align-content: stretch;
+    align-items: stretch;
+    flex-wrap: wrap;
+    margin: 10px auto;
+    max-width: 1400px;
+    margin-top: 60px;
+
+}
+.ranking-c {
+    display: inline-block;
+    max-width: 850px;
+    margin-left: 400px;
+    
+
+}
+
+.ranking {
+    display: flex; /* Adicionado */
+    background-color: #333;
+    border-radius: 10px;
+    padding: 20px;
+    margin: 10px;
+    text-align: justify;
+    width: 400px 900px;
+}
+
+p{
+	text-align: justify; 
+	margin-left: 50px;
+}
+h3 {
+    text-align: center;
+    color: #ff4500;
+    margin-top: 20px;
+    margin-right: 50px;
+}
+</style>
+<body>
+	<h1>Dados do Filme</h1>
+
+	<div class="ranking-container">
+
+	<div class="ranking">
+        <img src=https://th.bing.com/th/id/OIP.B4X2Gzt2QIDCB2ra2t0OtQHaK-?pid=ImgDet&rs=1 alt="Capa do filme Vingadores: Ultimato">
+        <div> <!-- Adicionado -->
+            <h3>Vingadores: Ultimato</h3>
+            <p>Arrecadação total - US$ 2,8 bilhões
+            <br>Arrecadação EUA - US$ 858,4 milhões (30,7%)
+            <br>Arrecadação Brasil - US$ 85,7 milhões
+            <br>Custos produção - US$ 356 milhões</p>
+            <p>Sinopse:</p>
+            <p>Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. Com Tony Stark vagando perdido no espaço sem água nem comida, Steve Rogers e Natasha Romanov precisam liderar a resistência contra o titã louco.</p>
+        </div>
+
+
+	</div>    
+
+    <div class="ranking-container">
+
+    <div class="ranking">
+        <img src=https://image.tmdb.org/t/p/original/8Y7WrRK1iQHEX7UIftBeBMjPjWD.jpg>
+        <div> <!-- Adicionado -->
+            <h3>Avatar</h3>
+<p>Arrecadação total - US$ 2,923 bilhões
+<br>Arrecadação EUA - Dados não disponíveis
+<br>Arrecadação Brasil - Dados não disponíveis
+<br>Custos produção - US$ 237 milhões</p>
+<p>Sinopse:</p>
+<p>Avatar, dirigido por James Cameron, é um filme de ficção científica épico de 2009. É o primeiro filme da série Avatar. Ambientado em meados do século 22, quando os humanos estão colonizando Pandora, uma lua habitável de um gigante gasoso no sistema estelar Alpha Centauri, para minerar o valioso mineral unobtanium. A expansão da colônia de mineração ameaça a existência contínua de uma tribo local de Na'vi, uma espécie humanoide indígena a Pandora.</p>
+
+        </div> <!-- Adicionado -->
+    </div>
+
+    <div class="ranking-container">
+
+    <div class="ranking">
+        <img src=https://th.bing.com/th/id/R.dad0169e533c061e0999285b3614a463?rik=bD7x1RbChOYasA&riu=http%3a%2f%2f2.bp.blogspot.com%2f-J_LatuSaoyk%2fT5wGZbNHc-I%2fAAAAAAAAFeY%2f1KVt3vEpX_4%2fs1600%2ftitanic%2bposter.jpg&ehk=t6iYS5WnHq8897gVebIMPTdKyT8PJd1HibXpZPwZVKI%3d&risl=&pid=ImgRaw&r=0>
+        <div> <!-- Adicionado -->
+            <h3>Titanic</h3>
+<p>Arrecadação total - US$ 2,187 bilhões
+<br>Arrecadação EUA - Dados não disponíveis
+<br>Arrecadação Brasil - Dados não disponíveis
+<br>Custos produção - US$ 200 milhões</p>
+<p>Sinopse:</p>
+<p>Titanic é um filme de romance e desastre americano de 1997 dirigido, escrito, co-produzido e co-editado por James Cameron. Uma história fictícia de dois passageiros a bordo do Titanic, Jack Dawson (Leonardo DiCaprio) e Rose DeWitt Bukater (Kate Winslet), que se apaixonam enquanto tentam sobreviver ao naufrágio do navio.</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+    <div class="ranking-container">
+
+    <div class="ranking">
+        <img src=https://th.bing.com/th/id/R.83ac5bbb93b82cf5232b1af8da26ce86?rik=snBXCfG1qLfAWg&pid=ImgRaw&r=0>
+        <div>
+        <h3>Star Wars</h3>
+<p>Arrecadação total - US$ 10,341 bilhões
+<br>Arrecadação EUA - Dados não disponíveis
+<br>Arrecadação Brasil - Dados não disponíveis
+<br>Custos produção - US$ 2.079 bilhões</p>
+<p>Sinopse:</p>
+<p>A franquia Star Wars envolve vários filmes de ação e aventura ao vivo e animados. A série de filmes começou com uma trilogia ambientada em medias res que foi posteriormente expandida para uma trilogia de trilogias, conhecida como a "Saga Skywalker".</p>
+        </div> <!-- Adicionado -->
+    </div>
+
+	
+	</div>
+</body>
+</html>
